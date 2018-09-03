6dab294c367541fd2af4b09da63fabfb4fa8cd03
Alain Green <maximo_ag1@hotmail.com>
2018-09-03 14:36:06 -0400

    Persist data using Codable protocol and plist

--- Get-Things-Done/Controller/TodoListViewController.swift
+++ Get-Things-Done/Controller/TodoListViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Go to town", "Do the dishes", "Do some coding"]
-    let defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+ 
+        loadItems()
     
     }
     
@@ -32,7 +30,14 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary Operator
+        //value = condition ? ValueIfTrue : ValueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -41,11 +46,9 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItem()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -60,9 +63,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
-            self.defaults.setValue(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItem()
         }
         
         alert.addTextField { (alertTextField) in
@@ -74,7 +80,33 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    //MARK - Save Items Function
+    
+    func saveItem() {
+        let encoder = PropertyListEncoder()
         
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    //MARK - Load Items
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array \(error)")
+            }
+        }
     }
     
     
---
+++ Get-Things-Done/DataModel/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Get-Things-Done
+//
+//  Created by AG on 9/3/18.
+//  Copyright © 2018 AG. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false 
+    
+}
