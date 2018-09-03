2a173d7113aa36368591d08f6d826dd1cba259d5
Alain Green <maximo_ag1@hotmail.com>
2018-09-03 00:24:26 -0400

    Initial Commit

---
+++ Get-Things-Done/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get-Things-Done
+//
+//  Created by AG on 9/3/18.
+//  Copyright © 2018 AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
